f44a19cea2d8fb89517ea5faca8f50b9277bd4b4
ふうき255 <luy18667@gmail.com>
2025-06-30 15:58:48 +0800

    myhtml_object.h の更新

--- myhtml_object.h
+++ myhtml_object.h
@@ -1,7 +1,7 @@
 #ifndef _MYHTML_OBJECT_H_
 #define _MYHTML_OBJECT_H_
 
-#define HTML_VERSION {2, 2, 1}
+#define HTML_VERSION {2, 3, 0}
 
 // Includes //
 
@@ -678,6 +678,25 @@ HtmlObject* HtmlCreateObjectComment(HtmlObject* parent, const char* text) {
 	HtmlObject* comment = HtmlLibCreateObject(HTML_TYPE_COMMENT, NULL, parent);
 	HtmlSetText(comment->innerText, text);
 	return comment;
+}
+
+
+// A html document that having <html> <head> <meta> <title> and <body>
+HtmlObject* HtmlCreateDocumentTemplate(const char* title) {
+  HtmlObject* doc = HtmlCreateDocument();
+  HtmlObject* tagHtml = HtmlCreateObjectTag(doc, "html");
+
+  // head
+  HtmlObject* tagHead = HtmlCreateObjectTag(tagHtml, "head");
+  HtmlObject* tagMeta = HtmlCreateObjectSingle(tagHead, "meta");
+  HtmlSetObjectAttrValue(tagMeta, "charset", "utf-8");
+
+  HtmlCreateObjectTag(tagHead, "title", title, NULL);
+
+  // body
+  HtmlObject* tagBody = HtmlCreateObjectTag(tagBody, "body");
+
+  return doc;
 }
 
 
